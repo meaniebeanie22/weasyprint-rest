a048c03c23cea0edba5385fc196ea695887b9818
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-29 12:37:02 +1100

    Update __main__.py

--- weasyprint_rest/__main__.py
+++ weasyprint_rest/__main__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 from .env import is_debug_mode
 from .app import app
-import os
 
 if __name__ == '__main__':
     app().run(debug=is_debug_mode(), port=os.getenv("PORT", default=5000))
