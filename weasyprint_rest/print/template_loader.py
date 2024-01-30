8b1903f4cb2b0b9ad191ac5f9426d248d306923e
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-30 14:07:28 +1100

    Update template_loader.py

--- weasyprint_rest/print/template_loader.py
+++ weasyprint_rest/print/template_loader.py
@@ -25,8 +25,6 @@ class TemplateLoader:
             self.template_definitions = {}
 
         def load(self, base_dir):
-            print(f'os.listdir() : {os.listdir()}')
-            print(f'os.listdir(\"weasyprint_rest\") : {os.listdir("weasyprint_rest")}')
             for template_dir in os.listdir(base_dir):
                 abs_template_dir = os.path.join(base_dir, template_dir)
                 template_file = os.path.join(abs_template_dir, "template.json")
