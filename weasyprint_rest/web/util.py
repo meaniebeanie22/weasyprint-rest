f5c8ac48abe5ebd85d4aed07e2a9e09d84a2be4e
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-30 16:30:19 +1100

    Update util.py

--- weasyprint_rest/web/util.py
+++ weasyprint_rest/web/util.py
@@ -20,12 +20,12 @@ def authenticate(func):
     @wraps(func)
     def verify_token(*args, **kwargs):
         try:
-            print(f'Correct API Key: {get_api_key()} Provided API Key: {request.headers['X_API_KEY']}', flush=True)
-            valid = secrets.compare_digest(get_api_key(), request.headers.get('X_API_KEY'))
+            print(f'Correct API Key: {get_api_key()} Provided API Key: {request.headers['X-API-KEY']}', flush=True)
+            valid = secrets.compare_digest(get_api_key(), request.headers.get('X-API-KEY'))
             print(f'Compare Digest: {valid}', flush=True)
             authenticated = (
                 get_api_key() is None
-                or ('X_API_KEY' in request.headers and get_api_key() == request.headers['X_API_KEY'])
+                or ('X-API-KEY' in request.headers and get_api_key() == request.headers['X-API-KEY'])
             )
             print(f'Authenticated: {authenticated}', flush=True)
         except Exception as error:  # pragma: no cover
