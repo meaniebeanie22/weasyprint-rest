b2936150d935d08a0267b7e2984fbfda188a2415
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-30 13:19:55 +1100

    Update util.py

--- weasyprint_rest/web/util.py
+++ weasyprint_rest/web/util.py
@@ -21,6 +21,7 @@ def authenticate(func):
     def verify_token(*args, **kwargs):
         try:
             print(f'Correct API Key: {get_api_key()} Provided API Key: {request.headers['X_API_KEY']}', flush=True)
+            print(f'Compare Digest: {secrets.compare_digest(get_api_key(), request.headers.get('X_API_KEY'))}')
             authenticated = (
                 get_api_key() is None
                 or ('X_API_KEY' in request.headers and get_api_key() == request.headers['X_API_KEY'])
